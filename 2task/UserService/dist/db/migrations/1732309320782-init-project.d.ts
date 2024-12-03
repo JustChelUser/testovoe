4c2525a119e96315af002e09dfce2d7b6b39ada5
justUser <eldar.konovalov@mail.ru>
2024-12-03 20:38:39 +0400

    удалена папка dist

--- 2task/UserService/dist/db/migrations/1732309320782-init-project.d.ts
+++
@@ -1,6 +0,0 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
-export declare class InitProject1732309320782 implements MigrationInterface {
-    name: string;
-    up(queryRunner: QueryRunner): Promise<void>;
-    down(queryRunner: QueryRunner): Promise<void>;
-}
--- 2task/UserService/dist/src/users/users.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { User } from './users.entity';
-import { Repository } from 'typeorm';
-export declare class UsersService {
-    private userRepository;
-    constructor(userRepository: Repository<User>);
-    updateUsersWithProblems(): Promise<number>;
-}
